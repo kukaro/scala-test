95acd03c40689d72dfc4237712f73d0872452320
coeat.k <coeat.k@kakaocorp.com>
2023-08-03 14:37:48 +0900

    feat:init

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+object Main {
+  var a = 10
+
+  def main(args: Array[String]): Unit = {
+    println("HI")
+    a_$eq(20)
+  }
+}
---
+++ src/main/scala/MainClass.scala
@@ -0,0 +1,11 @@
+class MainClass {
+  def main2(args: Array[String]): Unit = {
+   println("HELLO");
+  }
+}
+
+object MainClass{
+  def main(args: Array[String]): Unit = {
+    println("HELLO");
+  }
+}
