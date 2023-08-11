0ead1e8b350bfd2f78dd03ed22c43f7e01d4c453
coeat.k <coeat.k@kakaocorp.com>
2023-08-11 18:13:08 +0900

    feat: add match

---
+++ src/main/scala/pattern/Main.scala
@@ -0,0 +1,28 @@
+package pattern
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    example("hello")
+    example("world")
+    example("default")
+    example2("hello")
+    example2("world")
+    example2("default")
+  }
+
+  private def example(str: String): Unit = {
+    str match {
+      case _ if str.contains("hello") => println("hello")
+      case _ if str.contains("world") => println("world")
+      case _ => println("default")
+    }
+  }
+
+  private def example2(str: String): Unit = {
+    str match {
+      case "hello" => println("hello")
+      case "world" => println("world")
+      case _ => println("default")
+    }
+  }
+}
